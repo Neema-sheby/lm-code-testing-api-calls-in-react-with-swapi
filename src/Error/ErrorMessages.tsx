6a980aa3c406be1b42bd97ad063eeeb6aa4dc6ad
neema <neemadas@gmail.com>
2023-02-08 01:16:05 +0000

    refactor: Create an error message component.

---
+++ src/Error/ErrorMessages.tsx
@@ -0,0 +1,5 @@
+export const ErrorMessages = {
+  error500: "Error: 500 Oops... something went wrong, try again 🤕",
+  error418: "Error: 418 I'm a tea pot 😁, silly",
+  errorFetch: "Error: Oops... something went wrong, try again 🤕",
+};
