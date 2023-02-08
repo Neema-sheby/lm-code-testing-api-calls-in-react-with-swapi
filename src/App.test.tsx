01e18aa0f6b7e6705fdffd21854ca64aa0e31a69
neema <neemadas@gmail.com>
2023-02-08 00:09:59 +0000

    test: Setup server from msw

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 
 it("renders the heading", () => {
   render(<App />);
   const title = screen.getByText(/Star Wars/i);
   expect(title).toBeInTheDocument();
 });
+
+const handlers = [
+  rest.get("https://swapi.py4e.com/api/people", (req, res, ctx) => {
+    return res(
+      ctx.json({ results: [{ name: "star wars 1" }, { name: "star wars 2" }] })
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
