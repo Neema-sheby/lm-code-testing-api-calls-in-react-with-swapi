cd467c9cf75d2d1661aa570ed237ccac21c80c18
neema <neemadas@gmail.com>
2023-02-08 01:19:55 +0000

    test: Test to check render of error message when error status 418.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,23 @@
+/////////////////////////////////////////////////////////////////////////////////////
+
 import { render, screen, waitFor, within } from "@testing-library/react";
 import App from "./App";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
+import { ErrorMessages } from "./Error/ErrorMessages";
+const { error500, error418 } = ErrorMessages;
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// Test 1 ///////////////////////////////////////////////////////////
 
 it("renders the heading", () => {
   render(<App />);
   const title = screen.getByText(/Star Wars/i);
   expect(title).toBeInTheDocument();
 });
+
+// server setup using msw
 
 const handlers = [
   rest.get("https://swapi.py4e.com/api/people", (req, res, ctx) => {
@@ -23,6 +33,8 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
+// Test 2 ///////////////////////////////////////////////////////////
+
 it("renders the first name in the listadd", async () => {
   render(<App />);
 
@@ -34,6 +46,8 @@ it("renders the first name in the listadd", async () => {
 
   expect(headings[0]).toHaveTextContent("star wars 1");
 });
+
+// Test 3 ///////////////////////////////////////////////////////////
 
 it("renders error message when error status 500", async () => {
   server.use(
@@ -47,7 +61,22 @@ it("renders error message when error status 500", async () => {
   await waitFor(() => screen.findByRole("alert"));
   const alert = screen.getByRole("alert");
 
-  expect(alert).toHaveTextContent(
-    "Error: 500 Oops... something went wrong, try again 🤕"
+  expect(alert).toHaveTextContent(error500);
+});
+
+// Test 4 ///////////////////////////////////////////////////////////
+
+it("renders error message when error status 418", async () => {
+  server.use(
+    rest.get("https://swapi.py4e.com/api/people", (req, res, ctx) => {
+      return res(ctx.status(418));
+    })
   );
+
+  render(<App />);
+
+  await waitFor(() => screen.findByRole("alert"));
+  const alert = screen.getByRole("alert");
+
+  expect(alert).toHaveTextContent(error418);
 });
